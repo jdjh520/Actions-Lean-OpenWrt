a3f0c2503465490244dffeed89b6d4daa139b55a
jdjh520 <57866086+jdjh520@users.noreply.github.com>
2023-04-15 17:30:08 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -17,7 +17,7 @@
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # Lienol/openwrt-package
-# sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/jdjh520/openwrt-packages.git' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
 
 
