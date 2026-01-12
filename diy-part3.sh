5ed9230a3321bdcd064101e0d5ed92c54afe4396
jdjh520 <57866086+jdjh520@users.noreply.github.com>
2026-01-12 12:53:35 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -20,4 +20,4 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/luci/bin/config_generat
 
 # 修改内核版本
 
-# sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/ramips/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/ramips/Makefile
