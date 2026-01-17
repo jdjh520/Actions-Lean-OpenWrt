d5effc65c81d5529dc8d5020ccc6ca3f0442ce78
jdjh520 <57866086+jdjh520@users.noreply.github.com>
2026-01-17 18:12:46 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-#=============================================================
-# https://github.com/Lancenas/Actions-Lean-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=============================================================
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 
-# fw876/helloworld
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Modify default IP
+sed -i 's/192.168.6.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# 默认开启wifi
+# sed -i 's/wireless.radio${devidx}.disabled=1/wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# Lienol/openwrt-package
-# sed -i '$a src-git lienol https://github.com/jdjh520/openwrt-packages.git' feeds.conf.default
-# sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
+# sed -i 's/wireless.${name}.disabled=1/wireless.${name}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
+# 修改内核版本
 
-# git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-# git clone --depth=1 https://github.com/lylus/luci-app-zerotier.git package/luci-app-zerotier
-# git clone --depth=1 https://github.com/jdjh520/luci-app-autoreboot.git package/luci-app-autoreboot
+# sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/ramips/Makefile
