694c73026b37c22462467451d6623f6009a26e2d
jdjh520 <57866086+jdjh520@users.noreply.github.com>
2026-02-06 18:37:08 +0800

    Create 25.12-part1.sh

---
+++ 25.12-part1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/Lancenas/Actions-Lean-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# Lienol/openwrt-package
+# sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+
+
+# git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+# git clone --depth=1 https://github.com/lylus/luci-app-zerotier.git package/luci-app-zerotier
+# git clone --depth=1 https://github.com/jdjh520/luci-app-autoreboot.git package/luci-app-autoreboot
